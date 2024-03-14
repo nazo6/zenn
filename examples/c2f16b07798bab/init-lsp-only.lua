e12c02ac7380d49ea88734c3f6cd3f1e4aacc29a
nazo6 <git@nazo6.dev>
2024-03-14 19:58:32 +0900

    update example

--- examples/c2f16b07798bab/init-lsp-only.lua
+++ examples/c2f16b07798bab/init-lsp-only.lua
@@ -14,17 +14,9 @@ vim.opt.runtimepath:prepend(lazypath)
 
 -- lazy.nvimを使ってプラグインをインストール
 require("lazy").setup({
-  -- lsp関係
   { "neovim/nvim-lspconfig" },
   { "williamboman/mason.nvim" },
   { "williamboman/mason-lspconfig.nvim" },
-
-  -- nvim-cmp関係
-  { "L3MON4D3/LuaSnip" },
-  { "hrsh7th/nvim-cmp" },
-  { "hrsh7th/cmp-nvim-lsp" },
-  { "hrsh7th/cmp-buffer" },
-  { "saadparwaiz1/cmp_luasnip" },
 }, {})
 
 -- LSPサーバアタッチ時の処理
@@ -56,33 +48,6 @@ require("mason").setup()
 require("mason-lspconfig").setup()
 require("mason-lspconfig").setup_handlers {
   function(server_name)
-    require("lspconfig")[server_name].setup {
-      capabilities = require("cmp_nvim_lsp").default_capabilities(),
-    }
+    require("lspconfig")[server_name].setup {}
   end,
-}
-
--- nvim-cmpの設定
-local cmp = require "cmp"
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      require("luasnip").lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert {
-    ["<C-p>"] = cmp.mapping.select_prev_item(),
-    ["<C-n>"] = cmp.mapping.select_next_item(),
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.close(),
-    ["<CR>"] = cmp.mapping.confirm { select = true },
-  },
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-  }, {
-    { name = "buffer" },
-  }),
 }
---
+++ examples/c2f16b07798bab/start.sh
@@ -0,0 +1,1 @@
+NVIM_APPNAME=nvim-1 nvim
